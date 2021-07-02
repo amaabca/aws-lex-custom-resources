22ff523bdfc71f7212251ba4c531d36afb0f2546
Jesse Doyle <jesse.doyle@ama.ab.ca>
2021-07-02 17:35:01 -0600

    chore(test-coverage): Slot Type Resources
    * Add full test coverage for the Lex V2 Slot Type custom resource
      handler.
    * Refactor the handler to include a lexical scope (curly braces)
      for switch statement cases so that we can declare the `response`
      constant for each case.
    * Prefer the use of the spread (`...`) operator for building
      SDK requests so we don't need to mutate a shared object.

---
+++ __fixtures__/v2/events/slot-type/index.ts
@@ -0,0 +1,11 @@
+import create from './create.json';
+import deleteEvent from './delete.json';
+import unknown from './unknown.json';
+import update from './update.json';
+
+export default {
+  create,
+  delete: deleteEvent,
+  unknown,
+  update,
+}
