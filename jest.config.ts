f5515dea8e3a01381e3fac093c8d28fd73bc3e60
Zoie Carnegie <zoie.carnegie@gmail.com>
2021-07-07 14:20:48 -0600

    ADO-25882 Fix lint issues
    🐘
    
    - Fixed a trailing comma issue in jest.config.ts file
    
    ADO LINK: https://dev.azure.com/AMA-Ent/AMA-Ent/_boards/board/t/Red%20Team/Stories/?workitem=25882

--- jest.config.ts
+++ jest.config.ts
@@ -5,22 +5,16 @@ const config: Config.InitialOptions = {
   coverageThreshold: {
     global: {
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
   errorOnDeprecated: true,
   verbose: true,
   globalSetup: '<rootDir>/__tests__/setup.ts',
   transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   testRegex: '/__tests__/*',
-  testPathIgnorePatterns: [
-    '<rootDir>/__tests__/setup.ts',
-  ],
-  moduleFileExtensions: [
-    'ts',
-    'js',
-    'json',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/__tests__/setup.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
 };
 
 export default config;
