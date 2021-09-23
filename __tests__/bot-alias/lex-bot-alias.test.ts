da724ddbaca0959f826708f80b9e845c5028c29e
Zach Shaw <zachthm@gmail.com>
2021-09-23 13:58:49 -0600

    Bot-Alias Tests * Create bot-alias tests for resource and resource retention policy

---
+++ __tests__/bot-alias/lex-bot-alias.test.ts
@@ -0,0 +1,38 @@
+import {
+  App,
+  CfnResource,
+  Stack,
+} from '@aws-cdk/core';
+
+import {
+  LexBotAlias,
+} from '../../src/';
+
+describe('LexBotAlias', () => {
+  const serviceToken = 'arn:partition:service:region:account-id:resource-type:resource-id';
+  const app = new App();
+  const scope = new Stack(app, 'Stack');
+  const instance = new LexBotAlias(
+    scope,
+    'Id',
+    serviceToken,
+    {
+      botAliasName: 'Test',
+      botAliasLocaleSettings: {
+        ['en_US']: {
+          enabled: true,
+        },
+      },
+    }
+  );
+
+  describe('attributes', () => {
+    it('has a cfn resource', () => {
+      expect(instance.resource).toBeInstanceOf(CfnResource);
+    });
+
+    it('cfn resource has a removal policy of retain', () => {
+      expect(instance.resource.cfnOptions.deletionPolicy).toEqual('Retain');
+    });
+  });
+});
